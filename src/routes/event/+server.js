20b05e188fffa83c850143e0df06184a7639a332
nikolaswise <nikolaswise@gmail.com>
2023-02-25 21:35:17 -0800

    wow good thing im writing all thse tests

--- src/routes/event/+server.js
+++ src/routes/event/+server.js
@@ -2,63 +2,67 @@ import context from '$lib/context'
 import jsonld from "jsonld"
 import { insert } from "$lib/query"
 
-function unpack({event, obj}) {
+export const _unpack = ({event, obj}) => {
   let keys = [...Object.keys(obj)]
+  let newEvent = Object.assign(event, {})
   keys.forEach(key => {
     if (key === 'pushbroom') {
       // can be abstracted with below but brain not right now
       try {
         let json = JSON.parse(obj[key])
-        event[`event`] = json
-        unpack({event: json, obj: json})
+        newEvent[`event`] = json
+        _unpack({event: json, obj: json})
       } catch {
-        event[`event`] = obj[key]
+        newEvent[`event`] = obj[key]
       }
     } else {
       let shortKey = key.replace('pushbroom', '').toLowerCase()
       // can be abstracted with above but brain not right now
       try {
         let json = JSON.parse(obj[key])
-        event[`event:${shortKey}`] = json
-        unpack({event: json, obj: json})
+        newEvent[shortKey] = json
+        _unpack({event: json, obj: json})
       } catch {
-        event[`event:${shortKey}`] = obj[key]
+        newEvent[shortKey] = obj[key]
       }
     }
   })
-  return event
+  return newEvent
 }
 
-export async function POST({ request }) {
+export const _handler = async (event_id, data) => {
   const date = new Date()
-  const q = await request.json()
-  const event_id = `urn:uuid:${crypto.randomUUID()}`
-
   // create event object
   let event = {
     '@context': context,
     type: "Event",
     id: event_id,
-    source: q.v,
+    source: data.v,
     datetime: date.toISOString(),
-    timestamp: Date.now()
+    timestamp: date.getTime()
   }
 
   // If event data is not an object, just attach
-  // If event data IS an object, unpack it
-  if (typeof q.e !== 'object') {
-    event[`event:value`] = q.e
+  // If event data IS an object, _unpack it
+  if (typeof data.e !== 'object') {
+    event[`event`] = data.e
   } else {
-    let keys = [...Object.keys(q.e)]
-    event = unpack({event, obj: q.e})
+    event = _unpack({event, obj: data.e})
   }
+  return await jsonld.toRDF(event, {format: 'application/n-quads'});
+}
 
-  // convert to triples
-  const event_quads = await jsonld.toRDF(event, {format: 'application/n-quads'});
-
-  // insert to store
-  await insert(event_quads)
-
+export async function POST({ request }) {
+  // get the ppost body
+  const data = await request.json()
+  // create uuid for event
+  const event_id = `urn:uuid:${crypto.randomUUID()}`
+  // convert get url to triples
+  let triples = await _handler(event_id, url)
+  console.log(triples)
+  // insert triples to store
+  await insert(triples)
+  // return 200 in response
   let response = new Response(200)
   response.headers.append('Access-Control-Allow-Origin', subgraph)
 
--- src/routes/event/event.test.js
+++ src/routes/event/event.test.js
@@ -1,7 +1,148 @@
-import { describe, it, expect } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
-describe('event test', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+const uuid = 'event'
+const id = `urn:uuid:${uuid}`
+const date = new Date(2023, 1, 24, 13)
+const datetime = date.toISOString()
+const timestamp = date.getTime()
+
+describe('Unpack nested and namespaced objects', () => {
+  beforeEach(() => {
+    vi.resetModules()
+  })
+
+  afterEach(() => {
+    vi.resetModules()
+  })
+
+  it('attaches events in the `pushbroom` namespace', async () => {
+    const { _unpack } = await import('./+server.js')
+
+    let event = {
+      key: 'val'
+    }
+
+    let obj = {
+      pushbroom: 'some string okay',
+      pushbroomDataAttr: 'found this on the DOM',
+      pushbroomObj: {
+        cool: 'string',
+        nested: {
+          string: 'neat'
+        }
+      }
+    }
+
+    let target = {
+      key: 'val',
+      event: 'some string okay',
+      'dataattr': 'found this on the DOM',
+      'obj': {
+        cool: 'string',
+        nested: {
+          string: 'neat'
+        }
+      }
+    }
+
+    let result = _unpack({event, obj})
+
+    expect(result).toStrictEqual(target);
+
+  })
+
+  it('Flattens a deeply nested data object to the event object', async () => {
+    const { _unpack } = await import('./+server.js')
+
+    let event = {
+      key: 'val'
+    }
+    let obj = {
+      wow: 420,
+      cool: {
+        nested: {
+          truy: {
+            madly: {
+              deeply: 'string'
+            }
+          }
+        }
+      }
+    }
+
+    let target = {
+      key: 'val',
+      'wow': 420,
+      'cool': {
+        nested: {
+          truy: {
+            madly: {
+              deeply: 'string'
+            }
+          }
+        }
+      }
+    }
+    let result = _unpack({event, obj})
+    expect(result).toStrictEqual(target);
   });
 });
+
+describe('Event request body to RDF Triples', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('converts an object into triples', async () => {
+    vi.setSystemTime(date)
+    let data = {
+      v: 'https://pushbroom.test',
+      e: 'some cool data string'
+    }
+
+    let target = `<urn:uuid:event> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://pushbroom.co/vocabulary#Event> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary#datetime> "2023-02-24T21:00:00.000Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary#source> <https://pushbroom.test> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary#timestamp> "1677272400000"^^<http://www.w3.org/2001/XMLSchema#integer> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary/customEvents#> "some cool data string" .
+`
+
+    const { _handler } = await import('./+server.js')
+    let triples = await _handler(id, data)
+    expect(triples).toBe(target);
+  })
+
+  it('converts an object with an event data object into triples', async () => {
+    vi.setSystemTime(date)
+    let data = {
+      v: 'https://pushbroom.test',
+      e: {
+        pushbroom: 'Default value',
+        pushbroomDataAttr: 'found this on the DOM',
+        another: 'value',
+        nested: {
+          "string": "wow"
+        }
+      }
+    }
+
+    let target = `<urn:uuid:event> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://pushbroom.co/vocabulary#Event> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary#datetime> "2023-02-24T21:00:00.000Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary#source> <https://pushbroom.test> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary#timestamp> "1677272400000"^^<http://www.w3.org/2001/XMLSchema#integer> .
+<urn:uuid:event> <https://pushbroom.co/vocabulary/customEvents#> "Default value" .
+<urn:uuid:event> <https://pushbroom.co/vocabulary/customEvents#another> "value" .
+<urn:uuid:event> <https://pushbroom.co/vocabulary/customEvents#dataattr> "found this on the DOM" .
+<urn:uuid:event> <https://pushbroom.co/vocabulary/customEvents#nested> _:b0 .
+_:b0 <https://pushbroom.co/vocabulary/customEvents#string> "wow" .
+`
+
+    const { _handler } = await import('./+server.js')
+    let triples = await _handler(id, data)
+    expect(triples).toBe(target);
+  })
+})
