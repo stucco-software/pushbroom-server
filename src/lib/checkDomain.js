2ec25ac8196e299ff060fd582b6723d38aace66f
nikolaswise <nikolaswise@gmail.com>
2023-03-16 22:23:26 -0700

    log dem pals

--- src/lib/checkDomain.js
+++ src/lib/checkDomain.js
@@ -2,16 +2,21 @@ import { error } from '@sveltejs/kit'
 import { queryBoolean } from "$lib/query"
 
 const checkDomain = async (domain) => {
+  console.log('check domain')
   if (!domain) {
     throw error(409, {
       message: 'Domain unindentified'
     })
   }
+  console.log(`ASK {
+      <${domain}> rdf:type <pushbroom:Domain>
+    }`)
   let domainValid =  await queryBoolean(`
     ASK {
       <${domain}> rdf:type <pushbroom:Domain>
     }
   `)
+  console.log(`result: ${domainValid}`)
   if (!domainValid) {
     throw error(401, {
       message: 'Unauthorized domain'
